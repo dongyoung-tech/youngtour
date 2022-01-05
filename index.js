51052c189605c0f28d8f975da582d1c7b633d8b2
dongyoung-tech <66237519+dongyoung-tech@users.noreply.github.com>
2022-01-05 17:25:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -28,39 +28,7 @@ setInterval("imgnumation()",4000);
 
 //----------버튼------------------
 
-function recommendapi(){
-    var areacode = [0,1,2,3,4,5,6,7,8,31,32,33,34,35,36,37,38,39];
-    var number = Math.floor(Math.random() * 17 + 1);
-    var contentscode = 12;
-    var xhr = new XMLHttpRequest();
-    var url = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList'; /*URL*/
-    var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'yX8wx5nzKb42wtBThegyX7gb6G3xUCPCMfbzNYF1Gf0p0nSUn9ZeynPzokq9GNLvrFLmqQVbU9%2FQz9LckJpQLw%3D%3D'; /*Service Key*/
-    queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('3'); /**/
-    queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1');
-    queryParams += '&' + encodeURIComponent('arrange') + '=' + encodeURIComponent('P'); /**/
-    queryParams += '&' + encodeURIComponent('MobileOS') + '=' + encodeURIComponent('ETC'); /**/
-    queryParams += '&' + encodeURIComponent('MobileApp') + '=' + encodeURIComponent('AppTest'); /**/
-    queryParams += '&' + encodeURIComponent('areaCode') + '=' + encodeURIComponent(areacode[number]);
-    queryParams += '&' + encodeURIComponent('contentTypeId') + '=' + encodeURIComponent(contentscode); /**/
-    xhr.open('GET', url + queryParams);
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4) {
-            var xml = this.responseXML;
-            var names = xml.getElementsByTagName('title');
-            var image = xml.getElementsByTagName('firstimage');
-            console.log(image);
-            for(var i = 0; i<3; i++){
-                var num = i;
-                document.querySelectorAll('.recommend_place img')[num].setAttribute('src',image[num].innerHTML);
-                document.querySelectorAll('.recommend_topic h3')[num].innerHTML = names[num].innerHTML;
-            }
-        }
-    };
 
-    xhr.send('');
-}
-
-recommendapi();
 
 var photo = document.querySelectorAll('.intro_img');
 
